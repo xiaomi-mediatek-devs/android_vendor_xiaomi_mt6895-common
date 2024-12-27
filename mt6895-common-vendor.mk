e30d9f9b7150207085b6564148bff16d5bcd048b
Giovanni Ricca <giovanniricca@proton.me>
2024-12-27 10:34:19 +0100

    mt6895-common: Sync with tree
    Change-Id: I5a3d0aa2086cbeb4d6c8ea682ca89873e25d6432

--- mt6895-common-vendor.mk
+++ mt6895-common-vendor.mk
@@ -210,7 +210,9 @@ PRODUCT_PACKAGES += \
     libaal_cust \
     libaal_key \
     libaal_mtk \
+    libadpcmdec_mtk \
     libaedv \
+    libalacdec_mtk \
     libalsautils-v31 \
     libapu_mdw \
     libapu_mdw_batch \
@@ -243,6 +245,11 @@ PRODUCT_PACKAGES += \
     libcodec2_mtk_c2store \
     libcodec2_mtk_vdec \
     libcodec2_mtk_venc \
+    libcodec2_soft_mtk_alacdec \
+    libcodec2_soft_mtk_apedec \
+    libcodec2_soft_mtk_imaadpcmdec \
+    libcodec2_soft_mtk_mp3dec \
+    libcodec2_soft_mtk_msadpcmdec \
     libcodec2_vpp_dolby_plugin \
     libcodec2_vpp_dump_mtk_yuv_plugin \
     libcodec2_vpp_gc_plugin \
@@ -284,6 +291,7 @@ PRODUCT_PACKAGES += \
     libmmagent \
     libmml \
     libmnetlink_v104 \
+    libmp3dec_mtk \
     libmsbc_mtk \
     libmtk-fusion-ril-prop-vsim \
     libmtk-ril \
